9a4162504ba61bd00cd9c82f462ab426df1e4e25
armandoltx <armando@armandoltx.com>
2018-06-13 10:17:55 +1000

    starting exercise 2

--- 5chapter-8/ext2/src/index.js
+++ 5chapter-8/ext2/src/index.js
@@ -1,8 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import PropTypes from 'prop-types';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+const Stamp = () => {
+  <div className="stamp">
+    STAMP
+  </div>
+}
+
+const Sender = ({ fromPerson}) => {
+  var { name, address1, address2 } = fromPerson;
+  return (
+    <div className="label">
+      <p className="name">{name}</p>
+      <p className="address1">{address1}</p>
+      <p className="address2">{address2}</p>
+    </div>
+  );
+}
+
+function Envelope({ fromPerson}) {
+  return(
+    <div className="envelop">
+      <Sender fromPerson={returnLabel} />
+      <Stamp />
+    </div>
+  );
+}
+
+const returnLabel = {
+  name: "Mr. Sender",
+  address1: "123 Fake St.",
+  address2: "Boston, MA 02118"
+}
+
+const recipietnLabel = {
+  name: "Mrs. Receiver",
+  address1: "123 Fake St.",
+  address2: "San Francisco, CA 94101"
+}
+
+
+ReactDOM.render(
+  <Envelope toPerson={recipientLabel} fromPerson={returnLabel} />,
+  document.getElementById('root')
+);
