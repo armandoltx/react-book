5486aa704e3557cddfb3825dca71515c3e2cc88c
armandoltx <armando@armandoltx.com>
2018-07-21 15:37:58 +1000

    starting calculator

---
+++ 8chapter11/2.calculator/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import CalculatorGa from './CalculatorGa';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CalculatorGa />
+      </div>
+    );
+  }
+}
+
+export default App;
