7d86c4bc1645d93070b994d94cb155144e4c8128
Armando Ruiz <armando.ruiz@industrie.co>
2018-08-17 09:06:10 +1000

    Created cartpage, itempage, item and static data

--- 9shopper/src/App.js
+++ 9shopper/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Nav from './Nav';
 import './App.css';
+import ItemPage from './ItemPage';
+import CartPage from './CartPage';
+import {items} from './static-data';
 
 class App extends Component {
 
   state = {
-    activeTab: 0
+    activeTab: 0,
+    cart: []
   };
 
   handleTabChange = (index) => {
@@ -14,18 +18,61 @@ class App extends Component {
     })
   }
 
+  handleAddToCart = (item) => {
+    this.setState({
+      cart: [...this.state.cart, item.id]
+    });
+  }
+
 
   renderContent() {
     switch(this.state.activeTab) {
       default:
-      case 0: return <span>Items</span>
-      case 1: return <span>Cart</span>
+      case 0: return (
+        <ItemPage
+          items={items}
+          onAddToCart={this.handleAddToCart}
+        />
+      )
+      case 1:
+      return this.renderCart();
     }
   }
+
+  renderCart() {
+    //Count how many of each item is in the cart
+    let itemCounts = this.state.cart.reduce((itemCounts, itemId) => {
+      itemCounts[itemId] = itemCounts[itemId] || 0;
+      itemCounts[itemId]++;
+      return itemCounts;
+    }, {});
+
+    //Create an array of items
+    let cartItems = Object.keys(itemCounts).map(itemId => {
+      // Find the item by its id
+      var item = items.find(item =>
+        item.id === parseInt(itemId, 10)
+      );
+    
+    // Create a new 'item' and add the 'count property
+    return {
+      ...item,
+      count: itemCounts[itemId]
+    }
+    });
+
+    return (
+      <CartPage items={cartItems} />
+    )
+  }
+
   render() {
     let {activeTab} = this.state;
     return (
       <div className="App">
+        <div>
+          {this.state.cart.length} items
+        </div>
         <Nav activeTab={activeTab} onTabChange={this.handleTabChange} />
         <main className="App-content">
           {this.renderContent()}
---
+++ 9shopper/src/static-data.js
@@ -0,0 +1,34 @@
+let items = [
+  {
+    id: 0,
+    name: "Apple iPad Mini 2 16GB",
+    description: 'An iPad like no other. 16GB, WIFI, 4G.',
+    price: 229.00
+  },
+  {
+    id: 1,
+    name: "Apple iPad Mini 2 32GB",
+    description: 'Even larger than the 16GB, WIFI, 4G.',
+    price: 279.00
+  },
+  {
+    id: 2,
+    name: "Canon T71",
+    description: 'DSLR camera with lots of megapixels.',
+    price: 749.99
+  },
+  {
+    id: 3,
+    name: "Apple Watch Sport",
+    description: 'A watch',
+    price: 345.00
+  },
+  {
+    id: 4,
+    name: "Apple Watch Silver",
+    description: 'A watch silver',
+    price: 545.00
+  }
+];
+
+export { items };
